42b88029bae2d7674f7291bdff68b5ecc890f8cf
herzliyaaa <herzliabarangan@gmail.com>
2023-03-06 19:14:45 +0800

    feat: Add react-router-dom for routes; Login Page
    Added react-router-dom for routing and navigation. Added Login Page for
    auth.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import { Products } from "./features/product/Product";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./features/auth/Login";
+import { Products } from "./features/product/Product";
 
 function App() {
   return (
-    <div className='App'>
-      <Products />
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/products" element={<Products />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
